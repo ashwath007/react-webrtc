f24be42354b9f1304f1827e2414f5c968b3b384b
vicky <srivicky2000@gmail.com>
2021-08-25 14:26:11 +0530

    added socket react client

--- rtcV1/rtcone/src/App.js
+++ rtcV1/rtcone/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import React,{useEffect} from 'react';
 import './App.css';
+import {connectWebSocket} from './utils/WebSocket/WebSocketConn'
 
 function App() {
+
+  useEffect(() => {
+    connectWebSocket()  
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        WebRTC here 
+      </h2>
     </div>
   );
 }
